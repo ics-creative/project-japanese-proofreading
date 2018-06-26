6d318396360ce9b1604b1c6a47c8ed06928deacb
ics-ikeda <ikeda@ics-web.jp>
2018-06-26 13:56:33 +0900

    tslint+prettierで整形

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,48 @@
 import * as path from "path";
 import { ExtensionContext, workspace } from "vscode";
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from "vscode-languageclient";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	const serverModule = context.asAbsolutePath(path.join("out", "server.js"));
-	const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-	const serverOptions: ServerOptions = {
-		run : { module: serverModule, transport: TransportKind.ipc },
-		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions },
-	};
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{scheme: "file", language: "plaintext"}, {scheme: "file", language: "markdown"}],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contain in the workspace
-			fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-		},
-	};
-	client = new LanguageClient("languageServerExample", "Language Server Example", serverOptions, clientOptions);
-	client.start();
+  const serverModule = context.asAbsolutePath(path.join("out", "server.js"));
+  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [
+      { scheme: "file", language: "plaintext" },
+      { scheme: "file", language: "markdown" },
+    ],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contain in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+  client = new LanguageClient(
+    "languageServerExample",
+    "Language Server Example",
+    serverOptions,
+    clientOptions,
+  );
+  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
-import {TextlintMessage, TextlintResult} from "@textlint/kernel";
+import { TextlintMessage, TextlintResult } from "@textlint/kernel";
 import * as path from "path";
-import {TextLintEngine} from "textlint";
+import { TextLintEngine } from "textlint";
 import {
-	createConnection, Diagnostic, DiagnosticSeverity, DidChangeConfigurationNotification, InitializeParams,
-	Position, ProposedFeatures, Range, TextDocument, TextDocuments,
+  createConnection,
+  Diagnostic,
+  DiagnosticSeverity,
+  DidChangeConfigurationNotification,
+  InitializeParams,
+  Position,
+  ProposedFeatures,
+  Range,
+  TextDocument,
+  TextDocuments,
 } from "vscode-languageserver";
 import * as vscode_uri from "vscode-uri";
 
@@ -16,114 +24,132 @@ const documents: TextDocuments = new TextDocuments();
 let hasConfigurationCapability: boolean | undefined = false;
 
 connection.onInitialize((params: InitializeParams) => {
-	console.log("initialize");
-	const capabilities = params.capabilities;
-	hasConfigurationCapability = capabilities.workspace && !!capabilities.workspace.configuration;
+  console.log("initialize");
+  const capabilities = params.capabilities;
+  hasConfigurationCapability =
+    capabilities.workspace && !!capabilities.workspace.configuration;
 
-	return {
-		capabilities: {
-			textDocumentSync: documents.syncKind,
-            completionProvider: {
-                resolveProvider: true,
-            },
-		},
-	};
+  return {
+    capabilities: {
+      textDocumentSync: documents.syncKind,
+      completionProvider: {
+        resolveProvider: true,
+      },
+    },
+  };
 });
 
 connection.onInitialized(() => {
-	console.log("initialized");
-	if (hasConfigurationCapability) {
-		connection.client.register(DidChangeConfigurationNotification.type, undefined);
-	}
+  console.log("initialized");
+  if (hasConfigurationCapability) {
+    connection.client.register(
+      DidChangeConfigurationNotification.type,
+      undefined,
+    );
+  }
 });
 
-const defaultSettings: TextlintSettings = { maxNumberOfProblems: 1000 };
-let globalSettings: TextlintSettings = defaultSettings;
-const documentSettings: Map<string, Thenable<TextlintSettings>> = new Map();
+const defaultSettings: ITextlintSettings = { maxNumberOfProblems: 1000 };
+let globalSettings: ITextlintSettings = defaultSettings;
+const documentSettings: Map<string, Thenable<ITextlintSettings>> = new Map();
 
 connection.onDidChangeConfiguration((change) => {
-	console.log("onDidChangeConfiguration");
-	if (hasConfigurationCapability) {
-		// Reset all cached document settings
-		documentSettings.clear();
-	} else {
-		globalSettings = (change.settings.textlintConfig || defaultSettings) as TextlintSettings;
-	}
+  console.log("onDidChangeConfiguration");
+  if (hasConfigurationCapability) {
+    // Reset all cached document settings
+    documentSettings.clear();
+  } else {
+    globalSettings = (change.settings.textlintConfig ||
+      defaultSettings) as ITextlintSettings;
+  }
 
-	// Revalidate all open text documents
-	documents.all().forEach(validateTextDocument);
+  // Revalidate all open text documents
+  documents.all().forEach(validateTextDocument);
 });
 
-function getDocumentSettings(resource: string): Thenable<TextlintSettings> {
-	if (!hasConfigurationCapability) {
-		return Promise.resolve(globalSettings);
-	}
-	let result = documentSettings.get(resource);
-	if (!result) {
-		result = connection.workspace.getConfiguration({ scopeUri: resource, section: "textlintConfig" });
-		documentSettings.set(resource, result);
-	}
-	return result;
+function getDocumentSettings(resource: string): Thenable<ITextlintSettings> {
+  if (!hasConfigurationCapability) {
+    return Promise.resolve(globalSettings);
+  }
+  let result = documentSettings.get(resource);
+  if (!result) {
+    result = connection.workspace.getConfiguration({
+      scopeUri: resource,
+      section: "textlintConfig",
+    });
+    documentSettings.set(resource, result);
+  }
+  return result;
 }
 
 // Only keep settings for open documents
 documents.onDidClose((e) => {
-	documentSettings.delete(e.document.uri);
+  documentSettings.delete(e.document.uri);
 });
 
 // ドキュメントを初めて開いた時と内容に変更があった際に実行します。
 documents.onDidChangeContent((change) => {
-	validateTextDocument(change.document);
+  validateTextDocument(change.document);
 });
 
 // バリデーション（textlint）を実施
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-	// In this simple example we get the settings for every validate run.
-	const settings = await getDocumentSettings(textDocument.uri);
+  // In this simple example we get the settings for every validate run.
+  const settings = await getDocumentSettings(textDocument.uri);
 
-	const document = textDocument.getText();
-	const ext: string = path.extname(vscode_uri.default.parse(textDocument.uri).fsPath);
+  const document = textDocument.getText();
+  const ext: string = path.extname(
+    vscode_uri.default.parse(textDocument.uri).fsPath,
+  );
 
- console.log("validate!!");
+  console.log("validate!!");
 
-	const engine: TextLintEngine = new TextLintEngine({
-		configFile: path.resolve(__dirname, "../.textlintrc"),
-	});
+  const engine: TextLintEngine = new TextLintEngine({
+    configFile: path.resolve(__dirname, "../.textlintrc"),
+  });
 
-	const results: TextlintResult[] = await engine.executeOnText(document, ext);
-	const diagnostics: Diagnostic[] = [];
+  const results: TextlintResult[] = await engine.executeOnText(document, ext);
+  const diagnostics: Diagnostic[] = [];
 
-	if (engine.isErrorResults(results)) {
-		const messages: TextlintMessage[] = results[0].messages;
+  if (engine.isErrorResults(results)) {
+    const messages: TextlintMessage[] = results[0].messages;
 
-		const l: number = messages.length;
-		for (let i: number = 0; i < l; i++) {
-			const message: TextlintMessage = messages[i];
-			const text: string = message.ruleId ? `${message.message} (${message.ruleId})` : message.message;
-			const pos: Position = Position.create(Math.max(0, message.line - 1), Math.max(0, message.column - 1));
+    const l: number = messages.length;
+    for (let i: number = 0; i < l; i++) {
+      const message: TextlintMessage = messages[i];
+      const text: string = message.ruleId
+        ? `${message.message} (${message.ruleId})`
+        : message.message;
+      const pos: Position = Position.create(
+        Math.max(0, message.line - 1),
+        Math.max(0, message.column - 1),
+      );
 
-			const diagnostic: Diagnostic = {
-				severity: toDiagnosticSeverity(message.severity),
-				range: Range.create(pos, pos),
-				message: text,
-				source: "japanese-proofreading",
-				code: message.ruleId,
-			};
-			diagnostics.push(diagnostic);
-		}
-	}
+      const diagnostic: Diagnostic = {
+        severity: toDiagnosticSeverity(message.severity),
+        range: Range.create(pos, pos),
+        message: text,
+        source: "japanese-proofreading",
+        code: message.ruleId,
+      };
+      diagnostics.push(diagnostic);
+    }
+  }
 
-	// Send the computed diagnostics to VSCode.
-	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+  // Send the computed diagnostics to VSCode.
+  connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
 
 function toDiagnosticSeverity(severity) {
-	switch (severity) {
-			case 0: return DiagnosticSeverity.Information;
-			case 1: return DiagnosticSeverity.Warning;
-			case 2: return DiagnosticSeverity.Error;
-	}
-	return DiagnosticSeverity.Information;
+  switch (severity) {
+    case 0:
+      return DiagnosticSeverity.Information;
+    case 1:
+      return DiagnosticSeverity.Warning;
+    case 2:
+      return DiagnosticSeverity.Error;
+  }
+  return DiagnosticSeverity.Information;
 }
 
 // Make the text document manager listen on the connection
@@ -133,6 +159,6 @@ documents.listen(connection);
 // Listen on the connection
 connection.listen();
 
-interface TextlintSettings {
-	maxNumberOfProblems: number;
+interface ITextlintSettings {
+  maxNumberOfProblems: number;
 }

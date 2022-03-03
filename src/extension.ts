dbc367c93c197e71a576c048a28fc00c1540f0ed
ics-furudate <furudate@ics-web.jp>
2022-03-03 18:00:48 +0900

    HTML(.html)ファイル形式の対応

--- src/extension.ts
+++ src/extension.ts
@@ -23,6 +23,7 @@ export function activate(context: ExtensionContext) {
   const clientOptions: LanguageClientOptions = {
     // Register the server for plain text documents
     documentSelector: [
+      { scheme: "file", language: "html" },
       { scheme: "file", language: "latex" },
       { scheme: "file", language: "review" },
       { scheme: "file", language: "plaintext" },
